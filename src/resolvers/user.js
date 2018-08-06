4cccff1a6d7b8d79aabc22dde330da34f533649b
Isac <isac_asj91@hotmail.com>
2018-08-06 17:31:03 -0300

    add eslint

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,10 +1,12 @@
 import GithubService from '../services/github'
 
+const user = async (_, { login }) => {
+  const { data } = await GithubService.getUser(login)
+  return data
+}
+
 export default {
   Query: {
-    user: async (_, { login }) => {
-      const { data } = await GithubService.getUser(login)
-      return data
-    }
-  }
+    user,
+  },
 }
